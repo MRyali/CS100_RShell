57bffbad1f084688e9691922e6ce59f82eefa7d0
Kashyap Panda <kashyap.panda@gmail.com>
2020-02-11 22:46:52 -0800

    Added parseUserInput()
    Gets user input and splits based on spacing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iterator>
+#include <unistd.h>
 
 using namespace std;
+
+string* parseUserInput() {
+    string inputStr;
+    cout << "$ ";
+    getline(cin, inputStr);
+
+    istringstream buffer(inputStr);
+    istream_iterator<string> beginning(buffer), end;
+
+    vector<string> inputs(beginning, end);
+    return inputs.data();
+}
 
 int main() {
     
