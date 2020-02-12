44b05d283fd9e4843597f15e7d32fec63226034d
Kashyap Panda <kashyap.panda@gmail.com>
2020-02-11 23:48:17 -0800

    Fixed user input

--- src/main.cpp
+++ src/main.cpp
@@ -8,35 +8,32 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <vector>
 
 using namespace std;
 
-void execute(string *arr[]) {
+void execute(vector<char*> vectorInput) {
   // array for storing commands from user input
+  char** arr = &vectorInput[0];
   pid_t wait_for_result;
   int status;
+
   // fork child process
   pid_t child_process = fork();
 
   if (child_process == 0) {
     // execute command from array of strings
     execvp(arr[0], arr);
-    // wait for 1 second after executing command
-    sleep(1);
   }
   else {
       // wait for child process to finish
-      wait_for_result = (child_process, &status, WUNTRACED);
-      cout << "Fork() failed" << endl;
-      return;
+      wait_for_result = waitpid(child_process, &status, WUNTRACED);
   }
-  // wait 10 seconds
-  sleep(10);
   // kill child process
   kill(child_process, SIGKILL);
 }
 
-string* parseUserInput() {
+vector<char*> parseUserInput() {
     string inputStr;
     cout << "$ ";
     getline(cin, inputStr);
@@ -46,16 +43,23 @@ string* parseUserInput() {
         istream_iterator<string> beginning(buffer), end;
 
         vector<string> inputs(beginning, end);
-        return inputs.data();
+        vector<char*> returnInputs;
+        returnInputs.reserve(inputs.size());
+
+        for (int i = 0; i < inputs.size(); i++) {
+            returnInputs.push_back(const_cast<char*>(inputs[i].c_str()));
+        }
+
+        return returnInputs;
     }
     else {
-        return nullptr;
+        // return nullptr;
     }
 }
 
 int main() {
     while (true) {
-        string* input[] = parseUserInput();
+        vector<char*> input = parseUserInput();
         execute(input);
     }
 }
