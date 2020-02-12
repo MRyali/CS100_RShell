4b8266fe149fa2cc2d3f079360df49fde7fad902
Mayur Ryali <43820294+MRyali@users.noreply.github.com>
2020-02-11 22:48:55 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
 #include <iostream>
+#include <readline/readline.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 using namespace std;
 
+void fork_function() {
+  // array for storing commands from user input
+  string *arr[] = parseUserInput();
+  pid_t wait_for_result;
+  int status;
+  // fork child process
+  pid_t child_process = fork();
+
+  if (child_process == 0) {
+    // execute command from array of strings
+    execvp(arr[0], arr);
+    // wait for 1 second after executing command
+    sleep(1);
+  }
+  else {
+      // wait for child process to finish
+      wait_for_result = (child_process, &status, WUNTRACED);
+      cout << "Fork() failed" << endl;
+      return;
+  }
+  // wait 10 seconds
+  sleep(10);
+  // kill child process
+  kill(child_process, SIGKILL);
+}
+
 int main() {
-    
+
 }
