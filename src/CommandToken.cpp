2c1001f9cb54060fb977f6bcbe2a2c7b96d5a378
Mayur Ryali <43820294+MRyali@users.noreply.github.com>
2020-02-13 15:02:56 -0800

    Update CommandToken with execvp and fork functionality

--- src/CommandToken.cpp
+++ src/CommandToken.cpp
@@ -11,11 +11,34 @@ string CommandToken::getName() {
 bool CommandToken::execute() {
 	arguments.push_back(NULL);
 	char* arr[1000];
-	
+
 	int size = arguments.size();
 	for (int i = 1; i < size + 1; i++) {
 		arr[i] = arguments.front();
 		arguments.pop_front();
 	}
 	arr[0] = commandName;
+
+	//execvp/fork implementation
+
+	// array for storing commands from user input
+	pid_t wait_for_result;
+	int status;
+	// fork child process
+	pid_t child_process = fork();
+	//Error messages after fork updates
+	if (child_process < 0) {
+		perror("Fork Failed");
+		// terminates program upon recieving fork failure with return val of 1
+		exit(1);
+	}
+	else if (child_process == 0) {
+	  // execute command from our array of strings
+	  execvp(arr[0], arr);
+	}
+	else {
+		// wait for child process to finish
+		wait_for_result = waitpid(child_process, &status, WUNTRACED);
+	}
+	return true;
 }
