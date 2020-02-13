56470a2d6cc8159356935f3812fa22cf3995c963
Kashyap Panda <kashyap.panda@gmail.com>
2020-02-13 15:41:10 -0800

    Fixed logic errors within Tokenizer.cpp

--- src/Tokenizer.cpp
+++ src/Tokenizer.cpp
@@ -27,9 +27,9 @@ void Tokenizer::tokenize() {
         char* primitiveInput = new char[inputStr.size() + 1];
         strcpy(primitiveInput, inputStr.c_str());
         char* currToken = strtok(primitiveInput, " ");
-        
+
         bool firstTokenIsSemiColon = false;
-        
+
         if (containsSemiColon(currToken)) {
             currToken[strlen(currToken) - 1] = 0;
             char semicolonArray[] = ";";
@@ -37,30 +37,31 @@ void Tokenizer::tokenize() {
             connectors.push_back(semicolon);
             firstTokenIsSemiColon = true;
         }
-        
+
         if (!containsComment(currToken)) {
             while (currToken != 0 && !containsComment(currToken)) {
                 CommandToken* currCommand = new CommandToken(currToken);
                 char* nextToken = currToken;
-                
+
                 if (!containsComment(nextToken) && !firstTokenIsSemiColon) {
                     nextToken = strtok(NULL, " ");
-                    
+
                     while (nextToken != NULL && !containsComment(nextToken)) {
                         if (!containsComment(nextToken)) {
                             if (!containsOrAnd(nextToken)) {
                                 if (containsSemiColon(nextToken)) {
-                                    nextToken[strlen(currToken) - 1] = 0;
+                                    nextToken[strlen(nextToken) - 1] = 0;
+
+                                    currCommand->addArgument(nextToken);
+
                                     char semicolonArray2[] = ";";
                                     char* semicolon2 = semicolonArray2;
                                     connectors.push_back(semicolon2);
-                                    
-                                    currCommand->addArgument(nextToken);
                                     break;
                                 }
-                                
+
                                 if (containsComment(nextToken)) {
-                                   break; 
+                                   break;
                                 }
                                 else {
                                     currCommand->addArgument(nextToken);
@@ -70,24 +71,24 @@ void Tokenizer::tokenize() {
                                 if (containsComment(nextToken)) {
                                     break;
                                 }
-                                
+
                                 if (containsOrAnd(nextToken)) {
                                     connectors.push_back(nextToken);
                                 }
-    
+
                                 break;
                             }
                         }
                         nextToken = strtok(NULL, " ");
                     }
-                    
+
                     currToken = nextToken;
-                    currToken = strtok(NULL, currToken);
-                    
+                    currToken = strtok(NULL, " ");
+
                     if (currCommand->getName() == "exit") {
-                        commandTokens.push_back(currCommand);
                         ExitToken* exitToken = new ExitToken();
-                        
+                        commandTokens.push_back(exitToken);
+
                     }
                     else {
                         commandTokens.push_back(currCommand);
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(int argc, char *argv[]) {
 	while (true) {
 		string inputStr;
-		cout << "$ ";
+		cout << "🤡 ";
 		getline(cin, inputStr);
 
 		Tokenizer tokenizer = Tokenizer(inputStr);
