27037b2192bf8e015bd613c474fe1e1893a206ef
Mayur Ryali <43820294+MRyali@users.noreply.github.com>
2020-02-13 22:42:11 -0800

    Update bash tests with more commands

--- integration_tests/multiple_commands_tests.sh
+++ integration_tests/multiple_commands_tests.sh
@@ -2,4 +2,8 @@
 
 echo start && echo multipleCommands && echo tests
 ls -a; echo hello && mkdir test || echo world; git status
+ls; rm -rf test || git status
 ls -a; echo hello; mkdir test
+ls; rm -rf test; echo delete test
+echo final ; echo multiple command && echo test
+exit # this should exit
