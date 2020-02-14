30f1a0fc1df188bf6f96195135075bed24d423b3
Kashyap Panda <5233012+kpanda254@users.noreply.github.com>
2020-02-13 23:53:15 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(int argc, char *argv[]) {
 	while (true) {
 		string inputStr;
-		cout << "🍊 ";
+		cout << "$ ";
 		getline(cin, inputStr);
 
 		Tokenizer tokenizer = Tokenizer(inputStr);
