f3ac3c1419786f8b2d6d5fd9b0927d8c8f6011b7
Mayur Ryali <43820294+MRyali@users.noreply.github.com>
2020-02-27 19:59:38 -0800

    Add more shel script test commands

--- integration_tests/test_command_tests.sh
+++ integration_tests/test_command_tests.sh
@@ -9,3 +9,6 @@ test -d test/file/path;
 test -f src/Executor.cpp && echo "hello";
 test -d images || echo "world";
 test -f images || echo "world";
+test -f src/Executor.cpp && echo "hello"; #test with a comment
+test -e test/file/path && echo “path exists”; #another comment
+exit
