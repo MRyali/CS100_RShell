3dcf99073d96de2170131820a7b7e76633973795
Mayur Ryali <43820294+MRyali@users.noreply.github.com>
2020-02-28 19:03:56 -0800

    Add precedence tests and test bracket command tests

---
+++ integration_tests/precedence_command_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+(echo start && echo multipleCommands) && (echo tests && echo helloWorld)
+ls -a; (echo hello && mkdir test) || echo world; git status
+ls && (rm -rf test || git status)
+(ls -a) && (echo hello || mkdir test)
+(ls || rm -rf test) || echo delete test
+echo final || (echo multiple command && echo test)
+exit # this should exit
--- integration_tests/test_command_tests.sh
+++ integration_tests/test_command_tests.sh
@@ -13,4 +13,20 @@ test -f src/Executor.cpp && echo "hello"; #test with a comment
 test -e test/file/path && echo “path exists”; #another comment
 test -e test/file/path && echo “path exists” && ls; #one more comment
 test -e src && echo “path exists” || ls;
+
+[-e src] && echo “path exists” || ls;
+[-d test/file/path] && echo “path exists”;
+[names.txt];
+[-e names.txt];
+[-e test/file/path];
+[-f test/file/path];
+[-d test/file/path];
+[-f src/Executor.cpp] && echo "hello";
+[-d images] || echo "world";
+[-f images] || echo "world";
+[-f src/Executor.cpp] && echo "hello"; #test with a comment
+[-e test/file/path] && echo “path exists”; #another comment
+[-e test/file/path] && (echo “path exists” && ls); #one more comment
+[-e src] && (echo “path exists” || ls);
+
 exit
